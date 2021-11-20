2aaa8a3311f78e9f8c984d90bd4acf0418ddcd8b
Alu931 <alu931@hotmail.com>
2021-11-20 10:29:44 -0600

    Data Inicial

---
+++ Database/InitialData/ScriptConfiguration/Ordenes.sql
@@ -0,0 +1,45 @@
+﻿DROP TABLE IF EXISTS #OrdenTemp
+
+SELECT 
+IdOrden,IdProducto,CantidadProducto,Estado INTO #OrdenTemp
+FROM (
+VALUES
+(1,1,3,1),
+(2,1,2,1),
+(3,2,5,1),
+(4,2,7,1),
+(5,6,1,1),
+(6,4,12,0)
+)AS TEMP (IdOrden,IdProducto,CantidadProducto,Estado)
+
+
+----ACTUALIZAR DATOS---
+UPDATE T SET
+  T.IdProducto = TM.IdProducto,
+  T.CantidadProducto = TM.CantidadProducto,
+  T.Estado = TM.Estado
+FROM exp.Orden T
+INNER JOIN #OrdenTemp TM
+    ON T.IdOrden= TM.IdOrden
+
+
+----INSERTAR DATOS---
+
+SET IDENTITY_INSERT exp.Orden ON
+
+INSERT INTO exp.Orden(
+IdOrden,IdProducto,CantidadProducto,Estado )
+SELECT
+IdOrden,IdProducto,CantidadProducto,Estado
+FROM #OrdenTemp
+
+
+EXCEPT
+SELECT
+IdOrden,IdProducto,CantidadProducto,Estado
+FROM exp.Orden
+
+
+SET IDENTITY_INSERT exp.Orden OFF
+
+GO
---
+++ Database/InitialData/ScriptConfiguration/Productos.sql
@@ -0,0 +1,44 @@
+﻿DROP TABLE IF EXISTS #ProductTemp
+
+SELECT 
+IdProducto,NombreProducto,PrecioProducto INTO #ProductTemp
+FROM (
+VALUES
+(1,"COCA COLA 3LTS DESECHABLE",1850.00),
+(2,"COCA COLA LATA 354 ML",450.00),
+(3,"COCA COLA 2.5L RETORNABLE",1000.00),
+(4,"COCA-COLA DESECHABLE 600 ML",850.00),
+(5,"COCA COLA 1.5",1250.00),
+(6,"COCA COLA 2.5L DESECHABE",1500.00)
+)AS TEMP (IdProducto,NombreProducto,PrecioProducto)
+
+
+----ACTUALIZAR DATOS---
+UPDATE T SET
+  T.NombreProducto = TM.NombreProducto,
+  T.PrecioProducto = TM.PrecioProducto
+FROM exp.Producto T
+INNER JOIN #ProductTemp TM
+    ON T.IdProducto= TM.IdProducto
+
+
+----INSERTAR DATOS---
+
+SET IDENTITY_INSERT exp.Producto ON
+
+INSERT INTO exp.Producto(
+IdProducto,NombreProducto,PrecioProducto )
+SELECT
+IdProducto,NombreProducto,PrecioProducto
+FROM #ProductTemp 
+
+
+EXCEPT
+SELECT
+IdProducto,NombreProducto,PrecioProducto
+FROM exp.Producto
+
+
+SET IDENTITY_INSERT exp.Producto OFF
+
+GO

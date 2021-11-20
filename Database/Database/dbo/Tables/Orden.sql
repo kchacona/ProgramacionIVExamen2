4843ff070d03a2883d9ff528dee5b5b8c11c3b5d
Alu931 <alu931@hotmail.com>
2021-11-20 10:06:22 -0600

    Base SQL

---
+++ Database/Database/dbo/Tables/Orden.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE dbo.Orden
+(
+	IdOrden INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Orden PRIMARY KEY CLUSTERED(IdOrden),
+    IdProducto int NOT NULL
+	CONSTRAINT FK_Orden_Producto FOREIGN KEY(IdProducto) REFERENCES dbo.Producto(IdProducto),
+	CantidadProducto INT Not NULL,
+	Estado bit Not null
+)WITH (DATA_COMPRESSION = PAGE)
+GO
+
---
+++ Database/Database/dbo/Tables/Producto.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE dbo.Producto
+(
+IdProducto INT NOT NULL IDENTITY(1,1) CONSTRAINT
+PK_Producto PRIMARY KEY CLUSTERED(IdProducto)
+,NombreProducto VARCHAR(250) NOT NULL
+,PrecioProducto Float NOT NULL
+)
+WITH (DATA_COMPRESSION = PAGE)
+GO
+

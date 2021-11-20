2ba332ea0ae695a75176bf7dabf3dee3e1c4bee5
Alu931 <alu931@hotmail.com>
2021-11-20 10:53:32 -0600

    Store produce orden

---
+++ Database/Database/dbo/Store Procedures/OrdenActualizar.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE [dbo].[OrdenActualizar]
+    @IdOrden Int,
+	@IdProducto Int,
+	@CantidadProducto INT,
+	@Estado bit
+
+
+AS BEGIN
+SET NOCOUNT ON
+
+  BEGIN TRANSACTION TRASA
+
+    BEGIN TRY
+	
+	UPDATE dbo.Orden SET
+	 IdProducto = @IdProducto
+	,CantidadProducto = @CantidadProducto
+	,Estado = @Estado
+	WHERE 
+	       IdOrden=@IdOrden
+	
+	  COMMIT TRANSACTION TRASA
+	  SELECT 0 AS CodeError, '' AS MsgError
+
+  END TRY
+
+  BEGIN CATCH
+
+	   SELECT 
+			 ERROR_NUMBER() AS CodeError,
+			 ERROR_MESSAGE() AS MsgError
+   
+	   ROLLBACK TRANSACTION TRASA
+
+   END CATCH
+
+ END
---
+++ Database/Database/dbo/Store Procedures/OrdenObtener.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[OrdenObtener]
+      @IdOrden  int= NULL
+AS BEGIN
+  SET NOCOUNT ON
+
+  SELECT 
+     T.IdProducto,
+     T.CantidadProducto ,
+     T.Estado
+     
+    FROM dbo.Orden T
+    WHERE
+    (@IdOrden  IS NULL OR IdOrden =@IdOrden )
+
+END
